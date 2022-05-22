de289fbb9747fb91800e7d1985eb55f113b1380e
Md Rifat Hossain <mdrifathossain.mr@gmail.com>
2022-05-22 15:34:42 +0600

    home carousel done

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,13 +1,11 @@
 import React from 'react';
+import HomeCarousel from './HomeCarousel';
 
 const Home = () => {
     return (
-        <div>
-            <h1 className='text-4xl font-bold'>Home</h1>
-            <h2 className='text-3xl font-bold'>Home</h2>
-            <h4 className='text-2xl font-bold'>Home</h4>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed sit modi fuga sunt, vitae distinctio reprehenderit fugit ducimus dolore necessitatibus quod aliquid quibusdam minus deleniti eveniet ipsa autem saepe neque quae. Sit vel, eaque ratione enim cumque minima beatae ipsa veniam natus fugiat temporibus pariatur, aspernatur nihil odit ipsam minus?</p>
-        </div>
+        <>
+        <HomeCarousel/>
+        </>
     );
 };
 
---
+++ src/Pages/Home/HomeCarousel.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "./Style.css";
+import { EffectFade, Pagination, Autoplay } from "swiper";
+const HomeCarousel = () => {
+  return (
+    <>
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        effect={"fade"}
+        loop={true}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: true,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination,EffectFade ]}
+        loopFillGroupWithBlank={true}
+        className="mySwiper"
+      >
+        <SwiperSlide
+          style={{
+            background: `url(https://i.ibb.co/0qGWNQj/3-1.jpg)`,
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+            backgroundPosition: " center",
+          }}
+              >
+                  <div className="px-12 pt-24">
+                      <h3 className="text-4xl">Big Sale</h3>
+                      <h5 className="text-5xl mt-8 leading-[60px]">Hand Tools <br /> Circular Saw & Power Saw</h5>
+                      <button className="btn btn-primary  mt-8 text-white hover:bg-success">Shop Now</button>
+                  </div>
+        </SwiperSlide>
+        <SwiperSlide
+          style={{
+            background: `url(https://i.ibb.co/bLKdkDN/4-1.webp)`,
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+            backgroundPosition: " center",
+          }}
+        >
+           <div className="px-12 pt-24">
+                      <h3 className="text-4xl">Big Sale</h3>
+                      <h5 className="text-5xl mt-8 leading-[60px]">Hand Tools <br /> Circular Saw & Power Saw</h5>
+                      <button className="btn btn-primary  mt-8 text-white hover:bg-success">Shop Now</button>
+                  </div>
+        </SwiperSlide>
+      
+              
+      </Swiper>
+    </>
+  );
+};
+
+export default HomeCarousel;
